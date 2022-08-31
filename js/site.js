720ea6eb1495b322e99853da73873bed51dc58c2
Ian-Barton <bobbarbid.1@gmail.com>
2022-08-30 19:03:32 -0500

    Done My Half
    I used divs instead of tables like I was most likely supposed to.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,38 @@
-function HelloWorld() {
-    alert("Hello World");
+function getValues() {
+    document.getElementById("results").innerHTML = "";
+    let start = parseInt(document.getElementById("startValue").value);
+    let end = parseInt(document.getElementById("endValue").value);
+    getLogic(start, end);
+}
+function getLogic(start, end) {
+    let rowTemplate = "";
+    let rows = 0;
+    let bg_style = `colBG1`
+    for (let i = start; i <= end; i++) {
+        rows++;
+        if(i % 3 == 0 && i % 5 == 0) {
+            rowTemplate += `<div class="col-2 px-2 ${bg_style}">Fizz Bizz</div>`
+        }
+        else {
+            if(i % 3 == 0) {
+                rowTemplate += `<div class="col-2 px-2 ${bg_style}">Fizz</div>` 
+            }
+            else if(i % 5 == 0) {
+                rowTemplate += `<div class="col-2 px-2 ${bg_style}">Bizz</div>`
+            }
+            else {
+                rowTemplate += `<div class="col-2 px-2 ${bg_style}">${i}</div>`
+            }
+        }
+        if (rows % 10 == 0) {
+            bg_style = `colBG1`;
+        }
+        else if (rows % 5 == 0){
+            bg_style = `colBG2`;
+        }
+    }
+    document.getElementById("results").innerHTML += rowTemplate;
+}
+function display() {
+
 }
